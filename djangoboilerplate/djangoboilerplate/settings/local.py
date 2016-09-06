f223aac2964b4397a28091ecfa0287c5f9f8cf34
kelly <kelly@debian-personal>
2016-09-06 15:54:38 -0400

    es6 + react + django compiling and playing nice

---
+++ djangoboilerplate/djangoboilerplate/settings/local.py
@@ -0,0 +1,8 @@
+import os
+
+from .base import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '2u80ijfa0wje;oijgaoi(*owhojiwfoie;sjfda;liw*E((*ljgrhgao;sdjk'
+
---
+++ djangoboilerplate/watch.sh
@@ -0,0 +1,2 @@
+cd frontend
+node_modules/.bin/webpack --config webpack.config.js --watch
